1afb5cc4669cde6719af64c3250b6c563bee21a2
Rao Fahad <raofahad046@gmail.com>
2023-11-11 16:11:32 +0500

    initial commit

--- lib/lambda-layers-consumer-stack.ts
+++ lib/lambda-layers-consumer-stack.ts
@@ -1,16 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaLayersConsumerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaLayersConsumerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const { service, stage } = props?.tags!;
   }
 }
